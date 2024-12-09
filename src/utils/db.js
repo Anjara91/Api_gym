9d99ce892d46bcfd42f38488b5be31afaccc2824
Anjara91 <anjaraoramascarrillo@hotmail.com>
2024-12-09 20:31:01 +0000

    nueva

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,11 +3,13 @@ const mysql = require("mysql2");
 //createConnection
 
 const pool = mysql.createPool({
-    host: "localhost",
+    host: proccess.env.HOST_DB,
     user: process.env.USER_DB,
     password:process.env.PASS_DB,
     port: process.env.PORT_DB,
     database:process.env.DB
 });
+
+
 
 module.exports = pool.promise();
